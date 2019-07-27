5d8d849111066a30fe3178414f08f15be5dec662
krishnapotla” <krishnachaitanya.potla@gmail.com>
2019-07-27 16:26:56 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import Header from "./Header.js";
 import "./App.css";
 
-function App() {
-  let subscribers = [
-    {
-      id: 1,
-      name: "Shilpa Bhat",
-      phone: "8888888888"
-    },
-    {
-      id: 2,
-      name: "Srishti Gupta",
-      phone: "9999999999"
-    }
-  ];
-  return (
-    <Fragment>
-      <Header heading="Phone Directory" />
-      <div className="component-body-container">
-        <button className="custom-btn add-btn">Add</button>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    };
+  }
+  render() {
+    return (
+      <Fragment>
+        <Header heading="Phone Directory" />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
 
-        <div className="grid-container heading-container">
-          <span className="grid-item name-heading">Name</span>
-          <span className="grid-item phone-heading">Phone</span>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {this.state.subscribersListToShow.map(sub => {
+            return (
+              <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            );
+          })}
         </div>
-        {subscribers.map(sub => {
-          return (
-            <div key={sub.id} className="grid-container">
-              <span className="grid-item">{sub.name}</span>
-              <span className="grid-item">{sub.phone}</span>
-              <span className="grid-item action-btn-container">
-                <button className="custom-btn delete-btn">Delete</button>
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    );
+  }
 }
 
 export default App;
